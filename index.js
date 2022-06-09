0a8b335617b258b187807d7ace498550143190fa
sevillecarlos <sevillecarlos@gmail.com>
2022-06-09 17:07:09 -0600

    ✨ add front message

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.get("/", (_, res) => {
+  res.send("Express Server Up");
+});
+
 app.use(router);
+
 app.listen(port, (err) => {
+  console.log(`http://localhost:${port}`);
   if (err) throw err;
 });
